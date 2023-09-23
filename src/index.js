9acb55b614e6d29b47c17aff209fd7118a307406
OksanaSokal <sokaloksana@gmail.com>
2023-09-24 00:47:18 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = Math.abs(n);
+    }
+
+    let arr = String(n).split("");
+
+    let result = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr[0] == 0) {
+            return;
+        } else {
+            result.push(arr[i]);
+        }
+    }
+
+    return result.join("");
+};
